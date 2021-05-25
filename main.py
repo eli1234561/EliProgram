071edd331669a9b193c9839fa372a489a88bde4d
Eli54356 <>
2021-05-25 03:00:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+  
+from tkinter import *
+window=Tk()
+# add widgets here
+lbl=Label(window, text="The Mental Health Quiz", fg='Black', font=("Serif", 21))
+lbl.place(x=110, y=50)
+btn=Button(window, text="Press To Continue", fg='Blue') 
+btn.place(x=210, y=280)
+window.title('The Mental Health Quiz 2021')
+window.geometry("600x500+10+20")
+# Button for closing
+exit_button = Button(window, text="Exit", command=window.destroy) 
+
+window.mainloop()
